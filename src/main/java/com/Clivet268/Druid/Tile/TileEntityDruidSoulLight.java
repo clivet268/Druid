7c678a53c15f0d69e449a8a203e9f73e8aaca0ab
clivet268 <clivet268@gmail.com>
2022-01-09 23:21:34 -0500

    Druid Soul Light added

---
+++ src/main/java/com/Clivet268/Druid/Tile/TileEntityDruidSoulLight.java
@@ -0,0 +1,90 @@
+package com.Clivet268.Druid.Tile;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class TileEntityDruidSoulLight extends TileEntity implements ITickable
+{
+    @Nullable
+    private Potion primaryEffect;
+    public void update()
+    {
+        if (this.world.getTotalWorldTime() % 80L == 0L)
+        {
+            this.updateBeacon();
+        }
+    }
+
+    public void updateBeacon()
+    {
+        if (this.world != null)
+        {
+            this.addEffectsToPlayers();
+        }
+    }
+
+    private void addEffectsToPlayers()
+    {
+        if (!this.world.isRemote && this.primaryEffect != null)
+        {
+            int k = this.pos.getX();
+            int l = this.pos.getY();
+            int i1 = this.pos.getZ();
+            AxisAlignedBB axisalignedbb = (new AxisAlignedBB((double)k, (double)l, (double)i1, (double)(k + 1), (double)(l + 1), (double)(i1 + 1))).grow(60).expand(0.0D, (double)this.world.getHeight(), 0.0D);
+            List<EntityPlayer> list = this.world.<EntityPlayer>getEntitiesWithinAABB(EntityPlayer.class, axisalignedbb);
+
+            for (EntityPlayer entityplayer : list)
+            {
+                entityplayer.addPotionEffect(new PotionEffect(this.primaryEffect, 80, 0, true, true));
+            }
+        }
+    }
+    @Nullable
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        return new SPacketUpdateTileEntity(this.pos, 3, this.getUpdateTag());
+    }
+
+    public NBTTagCompound getUpdateTag()
+    {
+        return this.writeToNBT(new NBTTagCompound());
+    }
+
+    @SideOnly(Side.CLIENT)
+    public double getMaxRenderDistanceSquared()
+    {
+        return 65536.0D;
+    }
+
+    @Nullable
+    private static Potion isBeaconEffect(int p_184279_0_)
+    {
+        Potion potion = Potion.getPotionById(p_184279_0_);
+        return potion;
+    }
+
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        this.primaryEffect = isBeaconEffect(compound.getInteger("Primary"));
+    }
+
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        super.writeToNBT(compound);
+        compound.setInteger("Primary", Potion.getIdFromPotion(this.primaryEffect));
+        return compound;
+    }
+
+}
