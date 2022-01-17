e69cdc0419a1bfe6c6fcbc861a51f7db87291b15
clivet268 <clivet268@gmail.com>
2022-01-17 16:56:11 -0500

    Structure Gen Tweaked

--- src/main/java/com/Clivet268/Druid/World/Gen/WorldGenCustomStructures.java
+++ src/main/java/com/Clivet268/Druid/World/Gen/WorldGenCustomStructures.java
@@ -2,9 +2,13 @@ package com.Clivet268.Druid.World.Gen;
 
 import com.Clivet268.Druid.Util.Square;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockStoneBrick;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
@@ -16,7 +20,8 @@ import java.util.Random;
 public class WorldGenCustomStructures implements IWorldGenerator {
     public static final WorldGenStructure DRUID_HUT = new WorldGenStructure("druid_hut");
     public static final WorldGenStructure DRUID_WELL = new WorldGenStructure("druid_well");
-    public static final WorldGenStructure DRUID_TEMPLE = new WorldGenStructure("druid_temple");
+    public static final WorldGenStructure DRUID_TEMPLE = new WorldGenStructure("druid_main_temple");
+    public static final WorldGenStructure DRUID_TEMPLE_OG = new WorldGenStructure("druid_main_temple_overgrown");
 
     private static class SussyPiece{
         WorldGenStructure we;
@@ -27,57 +32,94 @@ public class WorldGenCustomStructures implements IWorldGenerator {
         }
     }
 
+    int rprev = 4;
+
     public static final SussyPiece DH = new SussyPiece(DRUID_HUT, 4);
     public static final SussyPiece DW = new SussyPiece(DRUID_WELL, 4);
     public static final SussyPiece DT = new SussyPiece(DRUID_TEMPLE, 6);
+    public static final SussyPiece DTOG = new SussyPiece(DRUID_TEMPLE_OG, 6);
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 
         if( world.provider.getDimension() == 0){
-            int x;
-            if (random.nextBoolean()) {
-                x = random.nextInt() + 6;
-            } else {
-                x = random.nextInt() - 6;
-            }
-            int z;
-            if (random.nextBoolean()) {
-                z = random.nextInt() + 6;
-            } else {
-                z = random.nextInt() - 6;
-            }
+                if (random.nextInt(200) == 0) {
+                    if (rprev <= 0) {
+                        rprev = 4;
+                        //System.out.println("yes");
+                        ArrayList<Square> xzes = new ArrayList<>();
+                        BlockPos posespose = new BlockPos(16 * chunkX, getGroundFromAbove(world, chunkX, chunkZ), 16 * chunkZ);
+                        Rotation trot = generateStructure(random.nextInt(100) == 1 ? DTOG.we : DT.we, world, random, chunkX, chunkZ, 0, -2, 0);
+                        //int[] e = rotahteay(0,9,0,10, trot);
+                        xzes.add(new Square(-DT.siz, -DT.siz, DT.siz, DT.siz));
+                        int ee, eee, eeee, eeeee;
 
-                if (random.nextInt(20) == 0) {
-                    //System.out.println("yes");
-                    ArrayList<Square> xzes = new ArrayList<>();
-                    Rotation trot = generateStructure(DT.we, world, random, chunkX, chunkZ, 0, 0, 0);
-                    //int[] e = rotahteay(0,9,0,10, trot);
-                    xzes.add(new Square(-DT.siz, -DT.siz, DT.siz, DT.siz));
-                    int ee,eee,eeee,eeeee;
+                        for (int i = random.nextInt(5) + 2; i > 0; i--) {
+                            boolean areanotfound = true;
+                            ee = random.nextBoolean() ? 1 : -1;
+                            int xx = (ee * (DT.siz + 3 + random.nextInt(19)));
+                            int xxx = xx + (ee * DH.siz);
+                            eee = random.nextBoolean() ? 1 : -1;
+                            int zz = (eee * (DT.siz + 3 + random.nextInt(19)));
+                            int zzz = zz + (eee * DH.siz);
+                            boolean checkeee = true;
+                            Square pepe = new Square(xx, zz, xxx, zzz);
+                            int checkout = 100;
+                            while (checkeee) {
+                                int eggw = 0;
+                                for (Square e : xzes) {
+                                    while (Square.ovelapsqmark(pepe, e)) {
+                                        int eeewef = (random.nextBoolean() ? 1 : -1) * (3 + random.nextInt(19));
+                                        int eeewefz = (random.nextBoolean() ? 1 : -1) * (3 + random.nextInt(19));
+                                        xx += eeewef;
+                                        xxx += eeewef;
+                                        zz += eeewefz;
+                                        zzz += eeewefz;
+                                        pepe = new Square(xx, zz, xxx, zzz);
+                                        eggw++;
+                                    }
+                                }
+                                checkeee = !(eggw == 0);
+                            }
 
-                    for(int i =random.nextInt(5) + 2; i > 0; i--) {
-                        boolean areanotfound = true;
-                        ee = random.nextBoolean() ? 1 : -1;
-                        int xx = (ee * (DT.siz + 3+ random.nextInt(19)));
-                        eee = random.nextBoolean() ? 1 : -1;
-                        int xxx = (eee * (DT.siz + 3 + random.nextInt(19)));
-                        eeee = random.nextBoolean() ? 1 : -1;
-                        int yy = (eeee * (DT.siz + 3+ random.nextInt(19)));
-                        eeeee = random.nextBoolean() ? 1 : -1;
-                        int yyy = (eeeee * (DT.siz + 3+ random.nextInt(19)));
+                            BlockPos posise = new BlockPos((chunkX * 16) + xxx, getGroundFromAbove(world, (chunkX * 16) + xxx, (chunkZ * 16) + zzz), (chunkZ * 16) + zzz);
+                            generateStructure(DRUID_HUT, world, random, chunkX, chunkZ, xx, 0, zz);
+                            while (((int) posise.distanceSq(new Vec3i(posespose.getX(), posise.getY() + 1, posespose.getZ()))) >= 64) {
 
+                                System.out.println(((int) posise.distanceSq(new Vec3i(posise.getX(), posespose.getY(), posespose.getZ()))));
+                                System.out.println(posise);
+                                world.notifyBlockUpdate(posise, world.getBlockState(posise), Blocks.STONEBRICK.getDefaultState().withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.MOSSY), 3);
+                                generateBlock(world, posise, Blocks.STONEBRICK.getDefaultState().withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.MOSSY));
+                                BlockPos internalYfindingPos = posise.add(posise.getX() > posespose.getX() ? -random.nextInt(5) : random.nextInt(5),
+                                        0,
+                                        (posise.getZ() > posespose.getZ() ? -random.nextInt(5) : random.nextInt(5)));
+                                internalYfindingPos.add(0, getGroundFromAbove(world, internalYfindingPos.getX(), internalYfindingPos.getZ()), 0);
+                                posise = internalYfindingPos;
+
+                            }
 
                         }
 
-                        //generateStructure(DRUID_HUT, world, random, chunkX, chunkZ, 0, 0, (pm * (12 + random.nextInt(7))));
-                    }
 
-                    for(int i =random.nextInt(2) + 1; i > 0; i--) {
-                        //pm = random.nextBoolean() ? 1 : -1;
-                        //generateStructure(DRUID_WELL, world, random, chunkX, chunkZ, (pm * (12 + random.nextInt(7))), 0, (pm * (12 + random.nextInt(7))));
+                        for (int i = random.nextInt(2) + 1; i > 0; i--) {
+                            //pm = random.nextBoolean() ? 1 : -1;
+                            //generateStructure(DRUID_WELL, world, random, chunkX, chunkZ, (pm * (12 + random.nextInt(7))), 0, (pm * (12 + random.nextInt(7))));
+                        }
                     }
                 }
+                else{
+                    rprev--;
+                }
+                }
 
+        }
+
+    public void generateBlock(World world, BlockPos pos, IBlockState bs){
+        world.setBlockState(pos, bs);
+
+
+    }
+        public void postGenMakePath(World world, BlockPos pos, IBlockState bs){
+            generateBlock(world, pos, bs);
         }
 
 
@@ -117,8 +159,8 @@ public class WorldGenCustomStructures implements IWorldGenerator {
     private Rotation generateStructure(WorldGenStructure generator, World world, Random random, int chunkx, int chunkz, int xoffset, int yoffset, int zoffset){
         //ArrayList<Class<?>> classList = new ArrayList<Class<?>>(Arrays.asList(classes));
 
-        int x = (chunkx * 16) + random.nextInt(15) + xoffset;
-        int z = (chunkz * 16) + random.nextInt(15) + zoffset;
+        int x = (chunkx * 16) + xoffset;
+        int z = (chunkz * 16) + zoffset;
         int y = getGroundFromAbove(world,x,z) + yoffset;
         BlockPos pos = new BlockPos(x,y,z);
 
@@ -145,11 +187,16 @@ public class WorldGenCustomStructures implements IWorldGenerator {
         int y = 255;
         boolean foundGround = false;
         while(!foundGround && y-->=0){
+
             Block blockAt = world.getBlockState(new BlockPos(x,y,z)).getBlock();
+            if(blockAt == Blocks.WATER || blockAt == Blocks.TALLGRASS){
+
+            }
             foundGround = (blockAt == Blocks.DIRT || blockAt == Blocks.GRASS || blockAt == Blocks.GRAVEL || blockAt.getUnlocalizedName().contains("dirt")
-                    || blockAt.getUnlocalizedName().contains("soil") || blockAt.getUnlocalizedName().contains("sand") || blockAt.getUnlocalizedName().contains("stone"));
+                    || blockAt.getUnlocalizedName().contains("soil") || blockAt.getUnlocalizedName().contains("sand"));
+
         }
-        return y;
+        return MathHelper.clamp(y, 1,256);
 
     }
 }
