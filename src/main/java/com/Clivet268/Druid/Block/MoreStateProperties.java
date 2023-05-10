1a850949a0cc4c52b274ad8ec2b7c6a090a7ca88
clivet268 <clivet268@gmail.com>
2023-05-10 18:01:52 -0400

    More state properties added

---
+++ src/main/java/com/Clivet268/Druid/Block/MoreStateProperties.java
@@ -0,0 +1,7 @@
+package com.Clivet268.Druid.Block;
+
+import net.minecraft.state.BooleanProperty;
+
+public class MoreStateProperties {
+    public static final BooleanProperty BEARING = BooleanProperty.create("bearing");
+}
