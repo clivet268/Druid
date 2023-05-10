6d564abc918f300d3d02095ed6fa0d8d7b3563e0
clivet268 <clivet268@gmail.com>
2023-05-10 18:01:03 -0400

    Plant attributes added

---
+++ src/main/java/com/Clivet268/Druid/Block/Attribute.java
@@ -0,0 +1,20 @@
+package com.Clivet268.Druid.Block;
+
+public enum Attribute {
+    Fire,
+    Water,
+    Lightning,
+    Contact,
+    Soft,
+    Smooth,
+    Sharp,
+    Tough,
+    Floating,
+    Heavy,
+    Time,
+    Sticky,
+    Pure,
+    Feeding;
+
+
+}
