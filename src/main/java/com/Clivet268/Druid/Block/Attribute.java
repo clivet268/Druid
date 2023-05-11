5145dcef5b4b5dce63b8402b4feb05145b6d77d4
clivet268 <clivet268@gmail.com>
2023-05-11 14:46:12 -0400

    Attributes have a string now for nbt data storage

--- src/main/java/com/Clivet268/Druid/Block/Attribute.java
+++ src/main/java/com/Clivet268/Druid/Block/Attribute.java
@@ -1,20 +1,35 @@
 package com.Clivet268.Druid.Block;
 
 public enum Attribute {
-    Fire,
-    Water,
-    Lightning,
-    Contact,
-    Soft,
-    Smooth,
-    Sharp,
-    Tough,
-    Floating,
-    Heavy,
-    Time,
-    Sticky,
-    Pure,
-    Feeding;
+    Fire("fire"),
+    Water("water"),
+    Lightning("lightning"),
+    Contact("contact"),
+    Soft("soft"),
+    Smooth("smooth"),
+    Sharp("sharp"),
+    Tough("tough"),
+    Floating("floating"),
+    Heavy("heavy"),
+    Time("time"),
+    Sticky("sticky"),
+    Pure("pure"),
+    Feeding("feeding"),
+    Purging("purging");
 
+
+    private final String text;
+
+    /**
+     * @param text name of attribute
+     */
+    Attribute(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 
 }
